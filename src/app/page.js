10da37905f608d7eedee64f837ece20f8edc501c
Claudiu <claudeady4579@gmail.com>
2023-06-26 22:42:08 +0300

    Prepared The Project.

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>this is a test branch</main>;
+  return <div>I am ready for this project to Bang!</div>;
 }
