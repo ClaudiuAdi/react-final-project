c61cf89be9c08cfb2b4bae1c6bd5736ca3d534de
Claudiu <claudeady4579@gmail.com>
2023-06-29 00:51:36 +0300

    Added Informative Section.

---
+++ src/app/utils/fonts.js
@@ -0,0 +1,16 @@
+import { Mr_De_Haviland, Prata, Poppins } from "next/font/google";
+
+export const haviland = Mr_De_Haviland({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const prata = Prata({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
