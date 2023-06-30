f055374cc235fc81982ed64a75bdffdf1e13b298
Claudiu <claudeady4579@gmail.com>
2023-07-01 00:04:42 +0300

    Added The Form And Other Styles.

--- src/app/components/Advantages.jsx
+++ src/app/components/Advantages.jsx
@@ -16,24 +16,26 @@ function Advantages() {
   const icons = [flashSharp, cashSharp, ribbonSharp, peopleSharp];
 
   return (
-    <div className={styles["advantages-section"]}>
-      <div className={styles["advantages-image-overlay"]}>
-        <h2 className={styles["advantages-title"] + " " + prata.className}>
-          Advantages
-        </h2>
-        <div className={styles.advantages + " " + poppins.className}>
-          {advantagesArray.map((advantage, index) => (
-            <div key={advantage.title} className={styles.advantage}>
-              <IonIcon
-                icon={icons.at(index)}
-                className={styles["advantage-icon"]}
-              />
-              <p className={styles["advantage-title"]}>{advantage.title}</p>
-              <p className={styles["advantage-description"]}>
-                {advantage.description}
-              </p>
-            </div>
-          ))}
+    <div className={styles["advantages-container"]}>
+      <div className={styles["advantages-section"]}>
+        <div className={styles["advantages-image-overlay"]}>
+          <h2 className={styles["advantages-title"] + " " + prata.className}>
+            Advantages
+          </h2>
+          <div className={styles.advantages + " " + poppins.className}>
+            {advantagesArray.map((advantage, index) => (
+              <div key={advantage.title} className={styles.advantage}>
+                <IonIcon
+                  icon={icons.at(index)}
+                  className={styles["advantage-icon"]}
+                />
+                <p className={styles["advantage-title"]}>{advantage.title}</p>
+                <p className={styles["advantage-description"]}>
+                  {advantage.description}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/app/components/BookingForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import styles from "./BookingForm.module.css";
+import hotelsJson from "../data/hotels.json";
+import reception_image from "../resources/reception2.jpg";
+import Image from "next/image";
+import { poppins } from "../utils/fonts";
+
+function BookingForm() {
+  const hotelsArray = JSON.parse(JSON.stringify(hotelsJson));
+
+  return (
+    <>
+      <div className={styles["title-section"]}>
+        <span className={styles.bar + " " + styles["first-bar"]}></span>
+        <span>Booking</span>
+        <span className={styles.bar + " " + styles["last-bar"]}></span>
+      </div>
+      <div className={styles["booking-form-section"]}>
+        <div className={styles["form-card"]}>
+          <img
+            src={reception_image.src}
+            alt="reception image"
+            className={styles["form-image"]}
+          />
+          <form action="" className={styles.form + " " + poppins.className}>
+            <div className={styles["form-inputs"]}>
+              <div className={styles["form-input-name"] + " " + styles.field}>
+                <label htmlFor="name">Full name</label>
+                <input type="text" id="name"></input>
+              </div>
+              <div className={styles["form-input-hotel"] + " " + styles.field}>
+                <label htmlFor="hotel">Hotel</label>
+                <select name="hotel" id="hotel">
+                  {hotelsArray.map((hotel) => (
+                    <option key={hotel.name} value={hotel.name}>
+                      {hotel.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles["form-grouped-inputs"]}>
+                <div className={styles["form-input-booking-date"]}>
+                  <label htmlFor="booking-date">Date</label>
+                  <input type="date" id="booking-date" />
+                </div>
+                <div className={styles["form-input-nights-number"]}>
+                  <label htmlFor="nights-number">Number of nights</label>
+                  <select name="nights-number" id="nights-number">
+                    <option value="10">10</option>
+                    <option value="5">5</option>
+                    <option value="3">3</option>
+                    <option value="2">2</option>
+                  </select>
+                </div>
+              </div>
+              <div
+                className={
+                  styles["form-input-guests-number"] + " " + styles.field
+                }
+              >
+                <label htmlFor="guests-number">Number of guests</label>
+                <input type="number" id="guests-number" />
+              </div>
+              <span className={styles["breakfast-input"] + " " + styles.field}>
+                <label htmlFor="breakfast">Breakfast</label>
+                <input
+                  type="checkbox"
+                  id="breakfast"
+                  className={styles.checkboxInput}
+                />
+              </span>
+            </div>
+            <button className={styles["button-submit"]}>SUBMIT</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BookingForm;
