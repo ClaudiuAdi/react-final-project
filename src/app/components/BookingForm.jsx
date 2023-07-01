80fe7cad8ee906992c2f12c7fe99e08212af5dec
Claudiu <claudeady4579@gmail.com>
2023-07-01 21:43:27 +0300

    Finished Booking Form.

--- src/app/components/BookingForm.jsx
+++ src/app/components/BookingForm.jsx
@@ -3,16 +3,28 @@ import styles from "./BookingForm.module.css";
 import hotelsJson from "../data/hotels.json";
 import reception_image from "../resources/reception2.jpg";
 import Image from "next/image";
-import { poppins } from "../utils/fonts";
+import { poppins, prata } from "../utils/fonts";
 import SectionTitle from "./SectionTitle";
 
 function BookingForm() {
   const hotelsArray = JSON.parse(JSON.stringify(hotelsJson));
 
   return (
-    <>
-      <SectionTitle title={"BOOKING"} />
+    <div>
+      {/* <SectionTitle title={"BOOKING"} /> */}
       <div className={styles["booking-form-section"]}>
+        <span className={styles["section-title-tag"] + " " + poppins.className}>
+          BOOK NOW
+        </span>
+        <h2
+          className={
+            styles["section-engaging-subtitle"] + " " + prata.className
+          }
+        >
+          Take Action and Make
+          <br />
+          Your Aspirations Come True
+        </h2>
         <div className={styles["form-card"]}>
           <img
             src={reception_image.src}
@@ -22,32 +34,63 @@ function BookingForm() {
           <form action="" className={styles.form + " " + poppins.className}>
             <div className={styles["form-inputs"]}>
               <div className={styles["form-input-name"] + " " + styles.field}>
-                <label htmlFor="name">Full name</label>
-                <input type="text" id="name"></input>
+                <input type="text" id="name" required />
+                <div className={styles["underline"]}></div>
+                <label className={styles["input-label"]} htmlFor="name">
+                  Full name
+                </label>
               </div>
               <div className={styles["form-input-hotel"] + " " + styles.field}>
-                <label htmlFor="hotel">Hotel</label>
-                <select name="hotel" id="hotel">
+                <select name="hotel" id="hotel" required>
+                  <option
+                    className={styles["select-hide-option"]}
+                    disabled
+                    selected
+                  />
                   {hotelsArray.map((hotel) => (
                     <option key={hotel.name} value={hotel.name}>
                       {hotel.name}
                     </option>
                   ))}
                 </select>
+                <div className={styles["underline"]}></div>
+                <label className={styles["input-label"]} htmlFor="hotel">
+                  Hotel
+                </label>
               </div>
               <div className={styles["form-grouped-inputs"]}>
-                <div className={styles["form-input-booking-date"]}>
-                  <label htmlFor="booking-date">Check in date</label>
-                  <input type="date" id="booking-date" />
+                <div
+                  className={
+                    styles["form-input-booking-date"] + " " + styles.field
+                  }
+                >
+                  <input type="date" id="booking-date" required />
+                  <div className={styles["underline"]}></div>
+                  <label
+                    className={styles["input-label"]}
+                    htmlFor="booking-date"
+                  >
+                    Check-in date
+                  </label>
                 </div>
-                <div className={styles["form-input-nights-number"]}>
-                  <label htmlFor="nights-number">Number of nights</label>
-                  <select name="nights-number" id="nights-number">
+                <div
+                  className={
+                    styles["form-input-nights-number"] + " " + styles.field
+                  }
+                >
+                  <select name="nights-number" id="nights-number" required>
                     <option value="10">10</option>
                     <option value="5">5</option>
                     <option value="3">3</option>
                     <option value="2">2</option>
                   </select>
+                  <div className={styles["underline"]}></div>
+                  <label
+                    className={styles["input-label"]}
+                    htmlFor="nights-number"
+                  >
+                    Number of nights
+                  </label>
                 </div>
               </div>
               <div
@@ -55,23 +98,26 @@ function BookingForm() {
                   styles["form-input-guests-number"] + " " + styles.field
                 }
               >
-                <label htmlFor="guests-number">Number of guests</label>
-                <input type="number" id="guests-number" />
+                <input type="number" id="guests-number" required />
+                <div className={styles["underline"]}></div>
+                <label
+                  className={styles["input-label"]}
+                  htmlFor="guests-number"
+                >
+                  Number of guests
+                </label>
               </div>
+
               <span className={styles["breakfast-input"] + " " + styles.field}>
                 <label htmlFor="breakfast">Breakfast</label>
-                <input
-                  type="checkbox"
-                  id="breakfast"
-                  className={styles.checkboxInput}
-                />
+                <input type="checkbox" id="breakfast" />
               </span>
             </div>
             <button className={styles["button-submit"]}>SUBMIT</button>
           </form>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
