be27fa18e9f041f0a8c8701cf200a51555fca325
Claudiu <claudeady4579@gmail.com>
2023-07-05 19:00:18 +0300

    Did Footer Improvments.

---
+++ src/app/components/Footer/FooterSocials.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Icon from "../Icon";
+import {
+  FacebookLogo,
+  InstagramLogo,
+  LinkedinLogo,
+} from "@phosphor-icons/react";
+
+const socialIcons = [
+  { logo: FacebookLogo, link: "https://www.facebook.com/chesscoders" },
+  {
+    logo: InstagramLogo,
+    link: "https://instagram.com/chesscoders?igshid=MTIzZWMxMTBkOA==",
+  },
+  {
+    logo: LinkedinLogo,
+    link: "https://www.linkedin.com/company/chesscoders/",
+  },
+];
+
+function FooterSocials(props) {
+  return (
+    <div className={props.className}>
+      {socialIcons.map((social, index) => (
+        <a href={social.link} key={index} target="_blank">
+          <Icon icon={social.logo} weight="regular" />
+        </a>
+      ))}
+    </div>
+  );
+}
+
+export default FooterSocials;
